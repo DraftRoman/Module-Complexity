1eb436551dc01a56cfa2adebe7cf5d018b20f71d
DraftRoman <roman.boxes@gmail.com>
2026-06-08 19:53:42 +0200

    refactored find_common_items, used set

--- Sprint-1/Python/find_common_items/find_common_items.py
+++ Sprint-1/Python/find_common_items/find_common_items.py
@@ -9,13 +9,26 @@ def find_common_items(
     """
     Find common items between two arrays.
 
-    Time Complexity:
-    Space Complexity:
-    Optimal time complexity:
+    Time Complexity:O(n*m*k) because we are iterating through both lists and then checking if the item is already in the common items list
+    Space Complexity:O(n) because we are storing the common items in a new list
+    Optimal time complexity: O(n+m) because we have to iterate through both lists at least once to find the common items
     """
-    common_items: List[ItemType] = []
-    for i in first_sequence:
-        for j in second_sequence:
-            if i == j and i not in common_items:
-                common_items.append(i)
-    return common_items
+    # common_items: List[ItemType] = []
+    # for i in first_sequence:
+    #     for j in second_sequence:
+    #         if i == j and i not in common_items:
+    #             common_items.append(i)
+    # return common_items
+
+
+    first_set = set(first_sequence)
+    second_set = set(second_sequence)
+    return list(first_set & second_set)
+
+    """
+    Time Complexity now: O(n+m) because we are iterating through both list once
+    Space Complexity now: O(n+m) because we are storing the common items in a new list and we are also creating two sets which take up space
+    Optimal time complexity: O(n+m) because we have to iterate through both lists at least once to find the common items
+    """
+
+
